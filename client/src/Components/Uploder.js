369f421d8f7539256d08029b5212cbfa119d4602
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2024-02-13 15:48:52 +0200

    Update Uploder.js

--- client/src/Components/Uploder.js
+++ client/src/Components/Uploder.js
@@ -1,8 +1,8 @@
 import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { FiUploadCloud } from "react-icons/fi";
-import Loader from "./Notification/Loader";
-import { uploadImagesService } from "../Redux/APIs/ImageUploadService";
+import { uploadImageservice } from "../Redux/APIs/ImageUploadService";
+import Loader from "./Notfications/Loader";
 
 function Uploder({ setImageUrl }) {
   const [loading, setLoading] = useState(false);
@@ -12,7 +12,7 @@ function Uploder({ setImageUrl }) {
     async (acceptedFiles) => {
       const file = new FormData();
       file.append("file", acceptedFiles[0]);
-      const data = await uploadImagesService(file, setLoading);
+      const data = await uploadImageservice(file, setLoading);
       setImageUrl(data);
     },
     [setImageUrl]
@@ -25,29 +25,29 @@ function Uploder({ setImageUrl }) {
     });
   return (
     <div className="w-full text-center flex-colo gap-6">
-      { loading ? (
+      {loading ? (
         <div className="px-6 w-full py-8 border-2 border-border border-dashed bg-dry rounded-md">
           <Loader />
         </div>
       ) : (
         <div
-          { ...getRootProps() }
+          {...getRootProps()}
           className="px-6 w-full py-8 border-2 border-border border-dashed bg-main rounded-md cursor-pointer"
         >
-          <input { ...getInputProps() } />
+          <input {...getInputProps()} />
           <span className="mx-auto flex-colo text-subMain text-3xl">
             <FiUploadCloud />
           </span>
-          <p className="text-sm mt-2">Перетягніть або виберіть зображення</p>
+          <p className="text-sm mt-2">Drag your image here</p>
           <em className="text-xs text-border">
-            { isDragActive
+            {isDragActive
               ? "Drop it like it's hot!"
               : isDragReject
-                ? "Unsupported file type..."
-                : "тільки .jpg та .png файли будуть прийняті" }
+              ? "Unsupported file type..."
+              : "only .jpg and .png files will be accepted"}
           </em>
         </div>
-      ) }
+      )}
     </div>
   );
 }

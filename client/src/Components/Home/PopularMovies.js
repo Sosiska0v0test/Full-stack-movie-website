9559c6531a216544bf4c49774d42947f6b61259a
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2024-02-15 22:00:43 +0200

    Update PopularMovies.js

--- client/src/Components/Home/PopularMovies.js
+++ client/src/Components/Home/PopularMovies.js
@@ -12,7 +12,7 @@ function PopularMovies({ isLoading, movies }) {
       <Titles title="Популярні" Icon={ BsCollectionFill } />
       {
         isLoading ? <Loader /> : movies?.length > 0 ? (
-          <div className='grid sm:mt-12 mt-7 xl:grid-cols-6 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 h-86 w-full'>
+          <div className='grid sm:mt-12 mt-7 xl:grid-cols-7 2xl:grid-cols-6 lg:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-3 h-86 w-full'>
             {
               movies?.slice(0, 8).map((movie, index) => (
                 <Movie key={ index } movie={ movie } />

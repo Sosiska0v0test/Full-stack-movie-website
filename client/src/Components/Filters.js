fbdaceb10247f0527d1efac59d384a4c41446d29
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2024-02-24 22:29:46 +0200

    Update Filters.js

--- client/src/Components/Filters.js
+++ client/src/Components/Filters.js
@@ -55,11 +55,11 @@ function Filters(props) {
   ];
 
   return (
-    <div className="my-6 bg-dry border text-dryGray border-gray-800 grid md:grid-cols-5 grid-cols-2 lg:gap-12 gap-2 rounded p-6">
+    <div className="my-6 bg-dry border text-dryGray border-main grid md:grid-cols-5 grid-cols-2 lg:gap-12 gap-2 rounded p-6">
       { Filter.map((item, index) => (
         <Listbox key={ index } value={ item.value } onChange={ item.onChange }>
           <div className="relative">
-            <Listbox.Button className="relative border border-gray-800  w-full text-white bg-main rounded-lg cursor-default py-4 pl-6 pr-10 text-left text-xs">
+            <Listbox.Button className="relative w-full text-white bg-main rounded-lg cursor-default py-4 pl-6 pr-10 text-left text-xs">
               <span className="block truncate">{ item.value.title }</span>
               <span className="absolute inset-y-0 right-0 flex items-center pointer-events-none pr-2">
                 <FaAngleDown className="h-4 w-4" aria-hidden="true" />
@@ -71,7 +71,7 @@ function Filters(props) {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white border border-gray-800 text-dryGray rounded-md shadow-lg max-h-60 py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white border text-dryGray rounded-md shadow-lg max-h-60 py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
                 { item.items.map((iterm, i) => (
                   <Listbox.Option
                     key={ i }

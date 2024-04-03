2abb07a6954dd20b2838a12de20ea68e534af7a3
Sosiska0v0test <117019284+Sosiska0v0test@users.noreply.github.com>
2024-04-03 20:31:37 +0300

    Update Axios.js

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: 'https://catalog-furniture.onrender.com/api/',
+  baseURL: 'https://topolya-films-blsp.onrender.com/api/',
 });
 
 export default Axios;
